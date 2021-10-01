e905b92d19db5f4c8b472c1f84ef8cc2e8f3e1be
Sanjula-Dulshan <it20253530@my.sliit.lk>
2021-10-01 18:54:08 +0530

    Films row done

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import "./Row.css";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row(props) {
   const [movies, setmovies] = useState([]);
@@ -19,6 +22,19 @@ function Row(props) {
   return (
     <div className="row">
       <h2>{props.title}</h2>
+
+      <div className="row_posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className={`row_poster ${props.isLargeRow && "row_posterLarge"}`}
+            src={`${base_url}${
+              props.isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
     </div>
   );
 }

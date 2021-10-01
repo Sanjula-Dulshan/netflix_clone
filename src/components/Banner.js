a1c5e3e0c3a6f01325172678cd6e6a3273957041
Sanjula-Dulshan <it20253530@my.sliit.lk>
2021-10-01 20:00:11 +0530

    banner data load

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,21 +5,51 @@ import requests from "../requests";
 function Banner() {
   const [movie, setmovie] = useState([]);
 
-  //   useEffect(() => {
-  //     async function fetchData() {
-  //       const request = await axios.get(requests.fetchActionMovies);
-  //       setmovie(
-  //         request.data.results[
-  //           Math.floor(Math.random() * request.data.results.length - 1)
-  //         ]
-  //       );
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchActionMovies);
+      setmovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
 
-  //       return request;
-  //     }
-  //     fetchData();
-  //   }, []);
-  //   console.log(movie);
-  return <header></header>;
+      return request;
+    }
+    fetchData();
+  }, []);
+  console.log(movie);
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url(
+        'https://image.tmdb.org/t/p/original/${movie?.backdrop_path}'
+      )`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner_content">
+        {/*title */}
+        <h1>
+          {movie?.title ||
+            movie?.original_title ||
+            movie?.name ||
+            movie?.original_name}
+        </h1>
+
+        {/*button*/}
+        <div className="banner_button">
+          <button className="banner_button">Play</button>
+          <button className="banner_button">My List</button>
+        </div>
+
+        {/*Description */}
+        <h1 className="banner_description">{movie?.overview}</h1>
+      </div>
+    </header>
+  );
 }
 
 export default Banner;
